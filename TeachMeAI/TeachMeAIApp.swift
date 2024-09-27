2cf265d5df6a0fff1b402498170b6f0cb2561e38
Kevin <kevincorvallis@gmail.com>
2024-09-26 22:57:19 -0700

    Initial Commit

---
+++ TeachMeAI/TeachMeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachMeAIApp.swift
+//  TeachMeAI
+//
+//  Created by Kevin Lee on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TeachMeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeachMeAITests/TeachMeAITests.swift
@@ -0,0 +1,17 @@
+//
+//  TeachMeAITests.swift
+//  TeachMeAITests
+//
+//  Created by Kevin Lee on 9/26/24.
+//
+
+import Testing
+@testable import TeachMeAI
+
+struct TeachMeAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
